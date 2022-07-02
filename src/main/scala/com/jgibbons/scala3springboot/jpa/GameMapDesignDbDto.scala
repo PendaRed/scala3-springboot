0869e0505ae40af650b6ccedccb8f90357f42bf9
njacobs5074 <nick@highlandcows.com>
2022-07-02 14:41:47 +0200

    feat: upgraded to Scala 3.1.3, added ability to save a game using a JSON POST

--- src/main/scala/com/jgibbons/scala3springboot/jpa/GameMapDesignDbDto.scala
+++ src/main/scala/com/jgibbons/scala3springboot/jpa/GameMapDesignDbDto.scala
@@ -2,13 +2,13 @@ package com.jgibbons.scala3springboot.jpa
 
 import java.sql.Timestamp
 import java.time.LocalDateTime
-import javax.persistence.{Entity, GeneratedValue, GenerationType, Id, Table}
+import javax.persistence.{ Column, Entity, GeneratedValue, GenerationType, Id, Table }
 import scala.beans.BeanProperty
 
 @Entity
-@Table(name="game_state")
+@Table(name = "game_state")
 case class GameMapDesignDbDto(
-  @BeanProperty var gamename: String,
+  @BeanProperty @Column(name = "game_name") gameName: String,
   @BeanProperty var json: String,
   @BeanProperty var status: String,
   @BeanProperty var version: Int = 0,
@@ -17,9 +17,9 @@ case class GameMapDesignDbDto(
   @BeanProperty var updatedAt: Timestamp = Timestamp.valueOf(LocalDateTime.now),
   @BeanProperty var updatedBy: String = null
 ) {
-  def this() = this(gamename=null, json=null, status=null)
+  def this() = this(gameName = null, json = null, status = null)
 
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   var id: Long = _
 }
--- src/main/scala/com/jgibbons/scala3springboot/jpa/GameMapRepository.scala
+++ src/main/scala/com/jgibbons/scala3springboot/jpa/GameMapRepository.scala
@@ -3,7 +3,7 @@ package com.jgibbons.scala3springboot.jpa
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-//trait GameMapRepository extends CrudRepository[GameMapDesignDbDto, Long]{
-trait GameMapRepository extends JpaRepository[GameMapDesignDbDto, Long]{
-  def findById(id:Long): java.util.Optional[GameMapDesignDbDto]
+trait GameMapRepository extends JpaRepository[GameMapDesignDbDto, Long] {
+  def findById(id: Long): java.util.Optional[GameMapDesignDbDto]
+  def save(gameMapDesignDbDto: GameMapDesignDbDto): Unit
 }
